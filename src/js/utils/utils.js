288d577f14d92d5b502a2a84fcbd3d29364331c6
Eunyeol <ney9083@gmail.com>
2022-03-20 23:49:01 +0900

    Feat: getRandomNumber 함수 구현

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -1,2 +1,3 @@
-export const getRandomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1)) + min;
+export const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
