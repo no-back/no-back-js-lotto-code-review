e33617367447d3f315cf92a210806c573a767fb6
Eunyeol <ney9083@gmail.com>
2022-03-20 18:34:31 +0900

    Feat: App 클래스 구현

---
+++ src/js/components/App.js
@@ -0,0 +1,27 @@
+import PurchasedLotto from "./PurchasedLotto.js";
+import InputPurchaseAmount from "./InputPurchaseAmount.js";
+import LottoTicket from "../model/LottoTicket.js";
+
+export default class App {
+  constructor() {
+    this.LottoTickets = [];
+
+    this.inputPurchaseAmount = new InputPurchaseAmount({
+      createLottoTickets: this.createLottoTickets.bind(this),
+    });
+    this.purchasedLotto = new PurchasedLotto({
+      lottoTickets: this.lottoTickets,
+    });
+  }
+
+  createLottoTickets(numbersOfLotto) {
+    this.setState({
+      lottoTickets: [numbersOfLotto].fill().map((v) => new LottoTicket()),
+    });
+  }
+
+  setState({ lottoTickets }) {
+    this.lottoTickets = lottoTickets;
+    this.purchasedLotto.setState({ lottoTickets: this.lottoTickets });
+  }
+}
