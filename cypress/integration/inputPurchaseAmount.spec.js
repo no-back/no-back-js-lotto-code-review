276a156419a3d99671c0fb4f51197fdf3981d28c
Eunyeol <ney9083@gmail.com>
2022-03-21 23:05:46 +0900

    Test: 로또 구매 금액 입력 테스트

--- cypress/integration/inputPurchaseAmount.spec.js
+++ cypress/integration/inputPurchaseAmount.spec.js
@@ -1,14 +1,39 @@
-/* 
-  form의 submit event를 통해 enter와 click event 효과를 모두 사용한다.
-*/
+import { ALERT_MESSAGE, LOTTO_PRICE } from "../../src/js/utils/constants.js";
 
 describe("구매 금액 입력", () => {
   beforeEach(() => {
     cy.visit("/");
   });
-  it("input 창에 가장 처음 0을 입력할 경우 값이 입력되지 않는다.", () => {});
-  it("input 창에 문자열을 입력 시 값이 입력되지 않는다.", () => {});
-  it("input 값이 없을 경우 엔터 키 입력 시 alert 경고를 띄운다", () => {});
-  it("입력된 금액이 1000원 미만의 금액이 입력된 경우 alert를 띄우고 input 창을 초기화 한다.", () => {});
-  it("입력된 금액이 1000원 초과일 경우, 1000 단위로 나누어 떨어지지 않는다면 alert로 거스름돈 금액을 알려주고 구매한 로또는 표시한다.", () => {});
+  it("input 창에 문자열을 입력 시 값이 입력되지 않는다.", () => {
+    cy.get(".purchase-form__input").type("abc");
+    cy.get(".purchase-form__input").should("have.text", "");
+  });
+
+  it("입력된 금액이 1000원 미만의 금액이 입력된 경우 클릭하더라도 로또를 구매할 수 없다.", () => {
+    const purchaseAmount = Math.floor(Math.random() * 1000);
+    console.log(purchaseAmount);
+    cy.get(".purchase-form__input").type(purchaseAmount);
+    cy.get(".purchase-form__button").click();
+    cy.get(".purchased-lotto-section").should("have.css", "display", "none");
+  });
+  it("입력된 금액이 1000원 초과일 경우, 1000 단위로 나누어 떨어지지 않는다면 alert로 거스름돈 금액을 알려주고 구매한 로또는 표시한다.", () => {
+    const MIN = 1000;
+    const MAX = 100000;
+    const amountWithChange = Math.floor(Math.random() * (MAX - MIN)) + MIN;
+    const change = amountWithChange % LOTTO_PRICE;
+    const alertStub = cy.stub();
+
+    cy.on("window:alert", alertStub);
+    cy.get(".purchase-form__input")
+      .type(amountWithChange)
+      .type("{enter}")
+      .then(() => {
+        if (change > 0) {
+          const actualMessage = alertStub.getCall(0).lastArg;
+          expect(actualMessage).to.equal(
+            ALERT_MESSAGE.PURCHASE_AMOUNT_HAS_CHANGE(change)
+          );
+        }
+      });
+  });
 });
