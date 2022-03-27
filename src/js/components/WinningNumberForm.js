4659f77db0acc093db9798127ca6e7a260e2b646
realhee <wlsgml5628@gmail.com>
2022-03-27 15:38:09 +0900

    Feat: 당첨 번호 입력 값 검증 결과 확인하기 버튼 기능

--- src/js/components/WinningNumberForm.js
+++ src/js/components/WinningNumberForm.js
@@ -1,3 +1,33 @@
+import { $, $$ } from "../dom.js";
+import Modal from "./Modal.js";
 export default class WinningNumberForm {
-  constructor() {}
+  constructor() {
+    this.$winningNumberForm = $("#winning-number-form");
+    this.$openResultModalButton = $(".open-result-modal-button");
+    this.initEventListener();
+  }
+
+  initEventListener() {
+    this.$winnigNumbers = $(".winning-number"); // inputs
+    this.$winningNumberContainer = $("#winning-numbers-container");
+
+    this.$winningNumberForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.showWinningResultModal();
+    });
+    this.$winningNumberContainer.addEventListener("input", (e) => {
+      // TODO : input 클릭 후 입력 시 preventDefault()
+      // validate 함수 분리
+      if (e.target.value.length >= e.target.maxLength) {
+        e.preventDefault(); // check
+        e.target.nextElementSibling === null
+          ? $(".bonus-number").focus()
+          : e.target.nextElementSibling.focus();
+      }
+    });
+  }
+  showWinningResultModal() {
+    const WinningResultModal = new Modal();
+    WinningResultModal.onModalShow();
+  }
 }
