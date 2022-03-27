59b548c21a2440d8124723ed73182e273f0242ea
realhee <wlsgml5628@gmail.com>
2022-03-27 17:53:11 +0900

    Feat: 당첨 번호 중복 여부 검증

--- src/js/components/WinningNumberForm.js
+++ src/js/components/WinningNumberForm.js
@@ -1,33 +1,66 @@
 import { $, $$ } from "../dom.js";
+import { NUM, MESSAGE } from "../const.js";
 import Modal from "./Modal.js";
 export default class WinningNumberForm {
   constructor() {
     this.$winningNumberForm = $("#winning-number-form");
-    this.$openResultModalButton = $(".open-result-modal-button");
+    this.$winningNumberContainer = $("#winning-numbers-container");
+    this.$winningNumbers = $$(".winning-number");
+    this.$bonusNumber = $(".bonus-number");
+    this.winningNumbers = [];
     this.initEventListener();
   }
 
   initEventListener() {
-    this.$winnigNumbers = $(".winning-number"); // inputs
-    this.$winningNumberContainer = $("#winning-numbers-container");
-
     this.$winningNumberForm.addEventListener("submit", (e) => {
       e.preventDefault();
       this.showWinningResultModal();
     });
     this.$winningNumberContainer.addEventListener("input", (e) => {
-      // TODO : input 클릭 후 입력 시 preventDefault()
+      // TODO : "input"이 적절한가
+      // TODO : input 클릭 후 입력 시 preventDefault()..
       // validate 함수 분리
       if (e.target.value.length >= e.target.maxLength) {
         e.preventDefault(); // check
         e.target.nextElementSibling === null
-          ? $(".bonus-number").focus()
+          ? this.$bonusNumber.focus()
           : e.target.nextElementSibling.focus();
       }
+      // TODO : 보너스번호 무한 입력 preventDefault()
     });
   }
   showWinningResultModal() {
-    const WinningResultModal = new Modal();
+    const winningNumberSet = new Set(
+      [...this.$winningNumbers].map((node) => {
+        return node.value;
+      })
+    );
+    const bonusValue = this.$bonusNumber.value;
+    winningNumberSet.add(bonusValue);
+
+    if (this.isDuplicatedWinningNumbers(winningNumberSet)) {
+      alert(MESSAGE.ALERT_LOTTO_DUPLICATED_NUMBER);
+      return;
+    }
+    this.createWinningNumbers(winningNumberSet);
+    this.compareLottoNumbers();
+
+    const WinningResultModal = new Modal(); // 모달창에 배열 데이터를 넘긴다.
     WinningResultModal.onModalShow();
   }
+
+  createWinningNumbers(winningNumberSet) {
+    this.winningNumbers = winningNumberSet; // TODO : Array <- Set?
+  }
+
+  isDuplicatedWinningNumbers(winningNumberSet) {
+    if (winningNumberSet.size !== NUM.LOTTO_NUM_COUNT + 1) return true;
+    else return false;
+  }
+
+  compareLottoNumbers() {
+    // 기존 로또티켓 번호와 당첨 번호를 비교한다
+    //(input 이벤트마다 당첨결과 객체를 업데이트? 결과 확인 시에만 업데이트? 실시간 상태변경의 필요성..)
+  }
 }
+// 다시 시작하기 버튼을 누르면 LottoApp 상태를 초기화한다.
