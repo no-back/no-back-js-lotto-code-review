623d7476d6f0ea6b44a137fcd880e54859a41b6d
Eunyeol <ney9083@gmail.com>
2022-03-29 04:03:40 +0900

    Feat: onChangeWinningNumberInput, validateWinningNumber, setState, renderCheckMessage 메서드 구현

--- src/js/components/WinningNumberInput.js
+++ src/js/components/WinningNumberInput.js
@@ -17,7 +17,90 @@ export default class WinningNumberInput {
     this.init();
   }
 
-  
+  init() {
+    this.$winningNumberForm.addEventListener(
+      "keyup",
+      this.onChangeWinningNumberInput.bind(this)
+    );
+    this.$openResultModalButton.addEventListener(
+      "click",
+      this.onShowModal.bind(this)
+    );
+  }
+
+  onChangeWinningNumberInput(e) {
+    if (e.target.type != "number") return;
+
+    const { winningNumbers, bonusNumber } = {
+      winningNumbers: [
+        ...e.currentTarget.querySelectorAll(".winning-number"),
+      ].map(($input) => $input.value),
+      bonusNumber: e.currentTarget.querySelector(".bonus-number").value,
+    };
+
+    const { isFulfilled } = this.validateWinningNumber(
+      [...winningNumbers, bonusNumber].filter((v) => v !== "").map((v) => +v)
+    );
+    this.setState({ isFulfilled });
+
+    if (!this.isFulfilled) return;
+
+    this.setState({
+      winningNumber: {
+        winningNumbers: winningNumbers.map((v) => +v),
+        bonusNumber: +bonusNumber,
+      },
+    });
+    this.updateWinningNumber(this.winningNumber);
+  }
+
+  validateWinningNumber(numbersWithoutBlank) {
+    if (hasReduplicatedElement(numbersWithoutBlank)) {
+      return {
+        isFulfilled: false,
+        checkMessage: WINNING_NUMBER_CHECK_MESSAGE.REDUPLICATED,
+      };
+    }
+
+    return {
+      isFulfilled: true,
+      checkMessage: WINNING_NUMBER_CHECK_MESSAGE.FULFILLED,
+    };
+  }
+
+  setState({ isFulfilled, checkMessage, winningNumber }) {
+    if (typeof isFulfilled === "boolean") {
+      this.isFulfilled = isFulfilled;
+    }
+
+    if (
+      typeof checkMessage === "string" &&
+      this.checkMessage !== checkMessage
+    ) {
+      this.checkMessage = checkMessage;
+      this.renderCheckMessage();
+    }
+
+    if (typeof winningNumber === "object") {
+      this.winningNumber = winningNumber;
+    }
+  }
+
+  renderCheckMessage() {
+    this.$winningNumberCheckMessage.textContent = this.checkMessage;
+
+    if (!this.isFulfilled) {
+      this.$winningNumberCheckMessage.classList.replace(
+        "text-green",
+        "text-red"
+      );
+      disable(this.$openResultModalButton);
+      return;
+    }
+
+    this.$winningNumberCheckMessage.classList.replace("text-red", "text-green");
+    enable(this.$openResultModalButton);
+  }
 }
 
 const hasReduplicatedElement = (list) => {
