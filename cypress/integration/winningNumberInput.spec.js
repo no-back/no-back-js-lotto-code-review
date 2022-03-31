105784ac12adaf1bfabfe9cb69437ef805d7e6b7
Eunyeol <ney9083@gmail.com>
2022-03-31 15:40:53 +0900

    Test: 결과 확인버튼 활성화 테스트

--- cypress/integration/winningNumberInput.spec.js
+++ cypress/integration/winningNumberInput.spec.js
@@ -47,7 +47,27 @@ describe("당첨 번호 입력 검사", () => {
       });
   });
 
-  i
+  it("6개의 당첨번호와 1개의 보너스 번호가 모두 정상 입력되어야 결과 확인 버튼이 활성화된다.", () => {
+    const winningNumberList = [1, 2, 3, 4, 5, 6];
+    const inValidBonusNumber = 7;
+    const { LESS_THEN_LENGTH } = WINNING_NUMBER_CHECK_MESSAGE;
+
+    cy.get('[data-input="winning-number-input"]').each(($el, index) => {
+      cy.wrap($el).type(winningNumberList[index]);
+      cy.get('[data-button="modal-open-button"]').should("be.disabled");
+      cy.get('[data-winning-number="check-message"]').should(
+        "have.text",
+        LESS_THEN_LENGTH
+      );
+      cy.get('[data-winning-number="check-message"]').should(
+        "have.class",
+        "text-red"
+      );
+    });
+    cy.get('[data-winning-number="bonus-number"]').type(inValidBonusNumber);
+    cy.get('[data-button="modal-open-button"]').should("not.be.disabled");
+  });
+  
 
 });
 
