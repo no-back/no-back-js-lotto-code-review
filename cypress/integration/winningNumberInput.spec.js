4316cc0f716fad02387f5f1ad53c48902f085fe0
Eunyeol <ney9083@gmail.com>
2022-03-31 15:40:21 +0900

    Test: 당첨번호 입력 자리수 검사

--- cypress/integration/winningNumberInput.spec.js
+++ cypress/integration/winningNumberInput.spec.js
@@ -27,7 +27,27 @@ describe("당첨 번호 입력 검사", () => {
     cy.submitValue(true);
   });
 
-  
+  it("로또를 구매하면 당첨번호를 입력할 수 있는 창이 나타난다.", () => {
+    cy.get("[data-winning-number='form']").should("be.visible");
+  });
+
+  it("당첨번호를 입혁할 수 있는 창이 나타날 경우, 결과 확인 버튼이 비활성화 되어 있다.", () => {
+    cy.get('[data-button="modal-open-button"]').should("be.disabled");
+  });
+
+  it("당첨번호를 2자리 이상 입력할 경우 마지막 input을 제외하고 다음 input으로 이동한다.", () => {
+    const inValidWinningNumberList = [12, 22, 23, 25, 26];
+    cy.get('[data-input="winning-number-input"]')
+      .then(($inputList) => {
+        return $inputList.slice(0, 4);
+      })
+      .each(($el, index) => {
+        cy.wrap($el).type(inValidWinningNumberList[index]);
+        cy.wrap($el).next().should("have.focus");
+      });
+  });
+
+  i
 
 });
 
